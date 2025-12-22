3b39a17458a1f9b2d11ed43b7d385b9eb8b9ed4c
tnpfcmis <aristochoclate@gmail.com>
2025-12-22 18:25:24 +0530

    Repository Update

---
+++ src/main/java/com/tnpfc/protean/API/Repository/ProteanConfigRepository.java
@@ -0,0 +1,11 @@
+package com.tnpfc.protean.API.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tnpfc.protean.API.DTO.protean_config;
+
+@Repository
+public interface ProteanConfigRepository extends JpaRepository<protean_config,String>{
+
+}
