bb42bf2c1d96c5837fb1a5794b5543032ceb0ae8
tnpfcmis <aristochoclate@gmail.com>
2025-12-22 16:31:58 +0530

    First Upload-

---
+++ src/main/java/com/tnpfc/protean/API/ProteanApiApplication.java
@@ -0,0 +1,13 @@
+package com.tnpfc.protean.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProteanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnpfc/protean/API/ProteanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnpfc.protean.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
