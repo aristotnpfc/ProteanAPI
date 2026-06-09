a0ceeec2d1bc71e9d17703ae3bd6aa877eaeee54
tnpfcmis <aristochoclate@gmail.com>
2026-06-09 17:22:06 +0530

    Changes for Response

--- src/main/java/com/tnpfc/protean/API/controller/BankAccountController.java
+++ src/main/java/com/tnpfc/protean/API/controller/BankAccountController.java
@@ -13,6 +13,8 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,110 +33,128 @@ import com.tnpfc.protean.API.Util.UtilityMAIN;
 @RestController
 @RequestMapping("/Bank")
 public class BankAccountController {
-	private static final Logger logger = LoggerFactory.getLogger(BankAccountController.class);
-	private static final String CONFIG = "config";
-	private static String BankUrl;
-	private static String privateKey,publicKey,entityId;
-	
-	
-	@Autowired
-	private UtilityMAIN MAIN;
-	
-	@Autowired	
-	private ProteanConfigRepository apirep;
-	
-	@Autowired
-	private ProteanEncryption ProTeanEncr;
-	
-	@Autowired
-	private ProteanDecryption proTeanDecry;
-	
-	static {
-		ResourceBundle bundle = ResourceBundle.getBundle(CONFIG);
-		BankUrl = bundle.getString("BankUrl");
-		privateKey 	= bundle.getString("privateKey");
-		publicKey 	= bundle.getString("publicKey");
-		entityId	= bundle.getString("BankEntity");
-	}
-	
-	@PostMapping("/VerifyBank")
-	public String VerifyBank(@RequestBody BankAccount bank) {
-		String plain_data="";
-		
-		try {
-		protean_config config = apirep.findByConfigtype("TnpfcAPI");
-		String AccessToken = "Bearer "+config.getAccess_token();
-		logger.info("AccessToken=>"+AccessToken);
-		String ReqId=MAIN.generateUniqueNumber();
-		
-		
-		JsonObject json = new JsonObject(); 
-		json.addProperty("entityId", entityId);
-		json.addProperty("programId", bank.getProgramid());
-		json.addProperty("requestId", ReqId);
-		json.addProperty("custIfsc", bank.getIfsccode());
-		json.addProperty("custAcctNo", bank.getAccountno());
-		json.addProperty("txnType",bank.getTxntype());
-		
-		String data= json.toString();
-		logger.info("Data:"+data);
-		
-		String JsonValue = ProTeanEncr.mainEncryption(data,publicKey,privateKey,ReqId);
-		logger.info(BankUrl);
-		logger.info(JsonValue);
-		URL url = new URL(BankUrl);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setRequestProperty("apikey", config.getApikey()); // Api Key
-        conn.setRequestProperty("Authorization", AccessToken); //Security Key
-        conn.setDoOutput(true);       
+    private static final Logger logger =
+            LoggerFactory.getLogger(BankAccountController.class);
 
-        
-        try (OutputStream os = conn.getOutputStream()) {
-            os.write(JsonValue.getBytes(StandardCharsets.UTF_8));
+    private static final String CONFIG = "config";
+
+    private static String BankUrl;
+    private static String privateKey, publicKey, entityId;
+
+    @Autowired
+    private UtilityMAIN MAIN;
+
+    @Autowired
+    private ProteanConfigRepository apirep;
+
+    @Autowired
+    private ProteanEncryption ProTeanEncr;
+
+    @Autowired
+    private ProteanDecryption proTeanDecry;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle(CONFIG);
+
+        BankUrl = bundle.getString("BankUrl");
+        privateKey = bundle.getString("privateKey");
+        publicKey = bundle.getString("publicKey");
+        entityId = bundle.getString("BankEntity");
+    }
+
+    @PostMapping("/VerifyBank")
+    public ResponseEntity<?> VerifyBank(@RequestBody BankAccount bank) {
+
+        HttpURLConnection conn = null;
+
+        try {
+
+            protean_config config =apirep.findByConfigtype("TnpfcAPI");
+            String accessToken ="Bearer " + config.getAccess_token();
+            String reqId = MAIN.generateUniqueNumber();
+            logger.info("Request Id : {}", reqId);
+
+            // REQUEST JSON
+            JsonObject json = new JsonObject();
+
+            json.addProperty("entityId", entityId);
+            json.addProperty("programId", bank.getProgramid());
+            json.addProperty("requestId", reqId);
+            json.addProperty("custIfsc", bank.getIfsccode());
+            json.addProperty("custAcctNo", bank.getAccountno());
+            json.addProperty("txnType", bank.getTxntype());
+            String requestData = json.toString();
+
+            logger.info("Request Data : {}", requestData);
+
+            // ENCRYPT
+            String encryptedRequest =ProTeanEncr.mainEncryption(requestData,publicKey,privateKey,reqId);
+
+            URL url = new URL(BankUrl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type","application/json");
+            conn.setRequestProperty("apikey",config.getApikey());
+            conn.setRequestProperty("Authorization",accessToken);
+            conn.setDoOutput(true);
+
+            // TIMEOUT SETTINGS
+            conn.setConnectTimeout(15000); // 15 seconds
+            conn.setReadTimeout(30000);    // 30 seconds
+
+            // SEND REQUEST
+            try (OutputStream os = conn.getOutputStream()) {
+                os.write(encryptedRequest.getBytes(StandardCharsets.UTF_8));
+            }
+
+            int responseCode = conn.getResponseCode();
+            logger.info("Response Code : {}", responseCode);
+
+            InputStream is = (responseCode >= 200 && responseCode < 300)? conn.getInputStream(): conn.getErrorStream();
+            StringBuilder response = new StringBuilder();
+
+            try (BufferedReader br =new BufferedReader(new InputStreamReader(is))) {
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    response.append(line);
+                }
+            }
+
+            logger.info("Encrypted Response : {}",response.toString());
+
+            // JSON MAPPING
+            ObjectMapper mapper = new ObjectMapper();
+            EmailPayLoad payload =mapper.readValue( response.toString(),EmailPayLoad.class);
+
+            // DECRYPT
+            String plainData =proTeanDecry.mainDecryption(payload.getData(),payload.getSymmetricKey(),payload.getHash(),privateKey);
+
+            logger.info("Final Response : {}", plainData);
+
+            // SAVE TO DB HERE
+
+            // RETURN CUSTOM RESPONSE
+            return ResponseEntity.ok(plainData);
+
         }
-        
-        InputStream is = (conn.getResponseCode() >= 200 && conn.getResponseCode() < 300)
-                ? conn.getInputStream()
-                : conn.getErrorStream();
+        catch (java.net.SocketTimeoutException e) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        StringBuilder response = new StringBuilder();
-        String line;
+            logger.error("API Timeout Error", e);
 
-        while ((line = br.readLine()) != null) {
-            response.append(line);
-        }       
-        
-        ObjectMapper mapper = new ObjectMapper();
-        
-        System.out.println("Response Code: " + response.toString());    
-      
-       
-        EmailPayLoad payload = mapper.readValue(response.toString(), EmailPayLoad.class);
-       
-        plain_data = proTeanDecry.mainDecryption(payload.getData(), payload.getSymmetricKey(), payload.getHash(), privateKey);
-        
-        System.out.println("Final Data: " + plain_data);
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		return plain_data;
-	}
-	
-	public String getRandomString()
-	{
-	 
-	 String randomStr = UUID.randomUUID().toString();
-	    while(randomStr.length() < 5) {
-	        randomStr += UUID.randomUUID().toString();
-	    }		    
-	    logger.info("Process_id for Email Push->"+randomStr.substring(0, 5).toUpperCase());		    
-	    return randomStr.substring(0, 5);
-	    
-	}
+            return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("Request timeout");
+
+        }
+        catch (Exception e) {
+
+            logger.error("Bank Verification Error", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unable to process request");
+        }
+        finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
 }
